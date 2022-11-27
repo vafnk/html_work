a379236024ecc2ed924cc24c772deb5ea475f0db
vafnk <valeriaafonina8608@gmail.com>
2022-11-27 13:49:28 +0600

    some fixes

---
+++ index.js
@@ -0,0 +1,20 @@
+function clickHandler() {
+  alert("Не тыкай на меня....");
+}
+function moveHandler(event) {
+  console.log("event", event);
+}
+function changeHandler(event) {
+  console.log("event", event);
+  let str = document.getElementById("inputObject");
+  alert(str.value);
+}
+
+setInterval(function () {
+  r = Math.floor(Math.random() * 256);
+  g = Math.floor(Math.random() * 256);
+  b = Math.floor(Math.random() * 256);
+  let s = document.getElementById("slider");
+  document.getElementById("slider").style.backgroundColor =
+    "rgb(" + r + "," + g + "," + b + ")";
+}, 1000);
